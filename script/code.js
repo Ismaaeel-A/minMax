f5c6726bf823e1cafc6f3111f074899082ab238c
Ismaaeel-A <ismaaeelahmed42@gmail.com>
2024-06-04 11:14:27 +0200

    d

---
+++ script/code.js
@@ -0,0 +1,7 @@
+//----------------------------------------------------------------------------
+function minMax() {
+    let arrMInMax = [3, 7, 88, 3, 7, 33, 78, 3, 228]
+    console.log(Math.min(...arrMInMax))
+    console.log(Math.max(...arrMInMax))
+}
+minMax()
